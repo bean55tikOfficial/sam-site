f3af8d1b80b12f3f330f5621d374387bcfb4ffb3
bean55tikOfficial <158489040+bean55tikOfficial@users.noreply.github.com>
2024-02-05 16:58:19 -0500

    Delete SamHub directory

--- SamHub/Code/Type/Js/main.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.burger');
-const drop = document.querySelector('.dropdown');
-
-menu.addEventListener('click', function() {
-  drop.classList.toggle('show');
-});
